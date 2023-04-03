1b571b8dec6401d417e2856ff7f95b69f0c69954
Jeff Gonzalez <jeff@hypertheory.com>
2023-04-03 13:04:16 -0400

    fixed config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,8 +41,6 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
@@ -69,7 +67,14 @@ const config = {
           alt: "My Site Logo",
           src: "img/logo.svg",
         },
-        items: [],
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "The Book",
+          },
+        ],
       },
       footer: {
         style: "dark",
